760892f8b77f2b1f9c039d0f65de6c266588d019
evansj <evansj@email.chop.edu>
2019-04-03 10:18:59 -0400

    Pull images w/ snakemake.

--- Snakefile.py
+++ Snakefile.py
@@ -1,4 +1,4 @@
-import json
+import json, glob
 
 IMAGE = '/mnt/isilon/dbhi_bfx/perry/projects/asif/images/'
 
@@ -23,8 +23,25 @@ rule build_image:
         img_src = 'docker://' + wildcards.image.replace(':', '\\:')
         shell('singularity build {img_dest}.simg {img_src} > {output}')
 
+# data/interim/ConvertPairedFastQsToUnmappedBamWf/16afb941-2d18-4036-a849-e868df7f895d/call-CreateFoFN/execution/NA12878_unmapped_bam.list
+# name w/ sample for future
 rule run:
     input:
         i = IMAGE + 'broadinstitute/gatk:4.1.1.0.log'
-    shell:
-        'sh run.sh'
+    output:
+        'data/interim/ubam.ls'
+    run:
+        shell('sh run.sh')
+        # cp bam ls
+        bam_ls = glob.glob('data/interim/ConvertPairedFastQsToUnmappedBamWf/*/call-CreateFoFN/execution/*.list')[0]
+        shell('cp {bam_ls} {output}')
+
+# image input
+# /mnt/isilon/dbhi_bfx/perry/projects/asif/cromwell_test/wdl/singularity-hpc-test/PublicPairedSingleSampleWf_perry.inputs.json
+rule run_gvcf:
+    input:
+        'data/interim/ubam.ls'
+    run:
+        # mk input
+        shell('sh run_gvcf.sh')
+        # cp output
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 module load singularity
 TMPDIR=/home/evansj/me/tmp/ \
-      java -Xmx15g -Xms15g -jar -Dconfig.file=hpc.conf \
+      java -Xmx20g -Xms20g -jar -Dconfig.file=hpc.conf \
       /mnt/isilon/dbhi_bfx/apps/bin/cromwell-36.jar run \
       paired-fastq-to-unmapped-bam.wdl \
       -i paired-fastq-to-unmapped-bam.inputs.json \
-      -m metadata.json &> test.er
+      -o generic.google-papi.options.json \
+      -m metadata.json &> test.err
